06942474c523571358704dd1258fcf849e591640
Jim Gabriel <btreemain@gmail.com>
2018-07-02 06:24:18 -0700

    license update

--- raspberry_pi/src/test_talk_listen_to_circuitplayground.py
+++ raspberry_pi/src/test_talk_listen_to_circuitplayground.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
-# Author: Jim Gabriel
-# License: Public Domain
+"""
+ * This file is part of the Pia-the-robot project, 
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Jim Gabriel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+"""
 #
 # the purpose of this is just to test the commmunication is working between the PI and the Circuitplayground express.
 # commands (i.e. object method names) can be sent to the circuitplayground over the serial port
